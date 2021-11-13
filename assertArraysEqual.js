9982dc1e5db11181783bc602776d672dc72f967c
Rajul Ramani <rajulramani@gmail.com>
2021-11-13 18:18:06 +0000

    assert Equal arrays and log the output

---
+++ assertArraysEqual.js
@@ -0,0 +1,16 @@
+const assertArraysEqual = function(array1, array2) {
+  let c = 0;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      c++;
+    }
+    else {
+      console.log("Assertion Failed:[" + array1 + "] !== [" + array2 + "]");
+    }
+  }
+  if (c === array1.length) {
+    console.log("Assertion Passed:[" + array1 + "] === [" + array2 + "]");
+  }
+};
+assertArraysEqual([1, 2], [1, 2]); //supposing the arrays to be of equal length
+assertArraysEqual([1, 2], [1, 8]);
