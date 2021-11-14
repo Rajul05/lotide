8526101d38f205c47f8c9045ad50a8ff668cf39f
Rajul Ramani <79838106+Rajul05@users.noreply.github.com>
2021-11-14 08:44:27 -0500

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -13,18 +13,15 @@ const eqArrays = function(array1, array2) {
    return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      c++;
-    }
-    else {
+    if (array1[i] !== array2[i]) {
       console.log('false');
       return false;
     }
   }
-  if (c === array1.length) {
+  
     console.log('true');
     return true;
-  }
+  
 };
 //assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 //assertEqual(eqArrays([6, 2, 3], [1, 2, 3]), true);
