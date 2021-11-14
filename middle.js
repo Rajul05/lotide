e904ff008f0f36cd2cf86ee1a35e06d0bbc105ae
Rajul Ramani <79838106+Rajul05@users.noreply.github.com>
2021-11-14 08:46:01 -0500

    Update middle.js

--- middle.js
+++ middle.js
@@ -17,22 +17,19 @@ const eqArrays = function(array1, array2) {
       return true;
     }
 };
-const assertArraysEqual = function(array1, array2) {
-    let c = 0;
-    if (array1.length !== array2.length){
-        return false;
+const assertArraysEqual = function(array1, array2){
+  let c = 0;
+  if (array1.length !== array2.length){
+    return false;
+}
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log("Assertion Failed:[" + array1 + "] !== [" + array2 + "]");
+      return false;
     }
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        c++;
-      }
-      else {
-        console.log("Assertion Failed:[" + array1 + "] !== [" + array2 + "]");
-      }
-    }
-    if (c === array1.length) {
-      console.log("Assertion Passed:[" + array1 + "] === [" + array2 + "]");
-    }
+  }
+  console.log("Assertion Passed:[" + array1 + "] === [" + array2 + "]");
+  return true;
 };
 const middle = function(array) {
   const middleElement = [];
