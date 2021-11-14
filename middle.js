a9cb892d779e6b804cc5dea9f2e3729e25e4e013
Rajul Ramani <79838106+Rajul05@users.noreply.github.com>
2021-11-14 09:13:32 -0500

    Update middle.js

--- middle.js
+++ middle.js
@@ -1,18 +1,17 @@
 const eqArrays = function(array1, array2) {
-    let c = 0;
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        c++;
-      }
-      else {
-        console.log('false');
-        return false;
-      }
+  if (array1.length !== array2.length){
+    console.log('false');
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log('false');
+      return false;
     }
-    if (c === array1.length) {
-      console.log('true');
-      return true;
-    }
+  }
+  
+    console.log('true');
+    return true;
 };
 const assertArraysEqual = function(array1, array2) {
   if (array1.length !== array2.length){
