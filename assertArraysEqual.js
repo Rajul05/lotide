c4874758d4697e6aa7252ddaeb5a75542be90994
Rajul Ramani <79838106+Rajul05@users.noreply.github.com>
2021-11-14 08:41:58 -0500

    Update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,13 @@
 const assertArraysEqual = function(array1, array2) {
   let c = 0;
-  if (array1.length !== array2.length){
-    return false;
-  }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      c++;
-    }
-    else {
+    if (array1[i] !== array2[i]) {
       console.log("Assertion Failed:[" + array1 + "] !== [" + array2 + "]");
+      return false;
     }
   }
-  if (c === array1.length) {
-    console.log("Assertion Passed:[" + array1 + "] === [" + array2 + "]");
-  }
+  console.log("Assertion Passed:[" + array1 + "] === [" + array2 + "]");
+  return true;
 };
 assertArraysEqual([1, 2], [1, 2]); //supposing the arrays to be of equal length
 assertArraysEqual([1, 2], [1, 8]);
