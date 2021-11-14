9ceb9cfbc5fa41743f56d917c7026b8fa4bf47d2
Rajul Ramani <79838106+Rajul05@users.noreply.github.com>
2021-11-14 06:07:52 -0500

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -9,6 +9,9 @@ const assertEqual = function(actual, expected) {
 };
 const eqArrays = function(array1, array2) {
   let c = 0;
+  if (array1.length !== array2.length){
+   return false;
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] === array2[i]) {
       c++;
