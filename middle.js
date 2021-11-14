2c6a5e0740612551c4907754b0672bc884334999
Rajul Ramani <79838106+Rajul05@users.noreply.github.com>
2021-11-14 08:51:40 -0500

    Update middle.js

--- middle.js
+++ middle.js
@@ -16,6 +16,7 @@ const eqArrays = function(array1, array2) {
 };
 const assertArraysEqual = function(array1, array2) {
   if (array1.length !== array2.length){
+    console.log("Assertion Failed:[" + array1 + "] !== [" + array2 + "]");
     return false;
 }
   for (let i = 0; i < array1.length; i++) {
