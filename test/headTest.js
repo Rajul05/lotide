c832b4166390569e216600882e7a360dc5d39506
Rajul Ramani <79838106+Rajul05@users.noreply.github.com>
2021-11-20 19:15:05 -0500

    Delete test directory

--- test/headTest.js
+++
@@ -1,5 +0,0 @@
-// TEST CODE
-const assertEqual = require('../assertEqual');
-const head = require('../head');
-assertEqual(head(["Lighthouse Labs", "Bootcamp"]),"Lighthouse Labs");
-assertEqual(head([5,6,7]), 5);
