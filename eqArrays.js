a06bb1e2367e259300510a1917d62acead9d6700
Rajul Ramani <79838106+Rajul05@users.noreply.github.com>
2021-11-20 19:25:16 -0500

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,28 +1,16 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("true");
-  }
-  else {
-    console.log("false");
-  }
-};
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length){
-    console.log('false');
+    //console.log('false');
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      console.log('false');
+      //console.log('false');
       return false;
     }
   }
-  
-    console.log('true');
-    return true;
+  //console.log('true');
+  return true;
 };
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-//assertEqual(eqArrays([6, 2, 3], [1, 2, 3]), true);
-eqArrays([1, 2], [1, 2]); //supposing the arrays to be of equal length
-eqArrays([1, 2], [1, 8]);
+module.exports = eqArrays;
