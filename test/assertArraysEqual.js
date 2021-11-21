f04753f95ac585cff7fe305e055cbf798b2d9f79
Rajul Ramani <79838106+Rajul05@users.noreply.github.com>
2021-11-20 20:38:53 -0500

    Create assertArraysEqual.js

---
+++ test/assertArraysEqual.js
@@ -0,0 +1,4 @@
+//TEST
+const assertArraysEqual = require('../assertArraysEqual');
+assertArraysEqual([1, 2], [1, 2]); //supposing the arrays to be of equal length
+assertArraysEqual([11, 2], [1, 2]);
