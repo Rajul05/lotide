e97ca0ccae7ff17f298efbcbe11f765ccc3e2927
Rajul Ramani <79838106+Rajul05@users.noreply.github.com>
2021-11-20 19:15:57 -0500

    Update head.js

--- head.js
+++ head.js
@@ -7,16 +7,4 @@ const head = function (arr) {
   return x;
   
 }
-head([9,4,5,6,7]);
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) { 
-    console.log("Assertion Passed:" + actual + " === " + expected);
-  }
-  else { 
-    console.log("Assertion Failed:" + actual + " !== " + expected);
-  }
-};
-// TEST CODE
-assertEqual(head(["Lighthouse Labs", "Bootcamp"]),"Lighthouse Labs");
-assertEqual(head([5,6,7]), 5);
+module.exports = head;
